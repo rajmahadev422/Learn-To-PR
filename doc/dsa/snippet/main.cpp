b2e20c2b79f9ca3921edda03f3838f5296d7cd32
rajmahadev422 <rajmahadev422@gmail.com>
2026-04-02 17:48:40 +0530

    add dsa folder

---
+++ doc/dsa/snippet/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i, n) for(int i = 0; i < int(n); i++)
+#define ll long long
+
+void solve() {
+  
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin>>t;
+  while(t--) solve();
+
+  return 0;
+};
